935de02361c2abf7c554e80bd8d290b44fb1a676
Best <141381218+KorawichManeeratana@users.noreply.github.com>
2024-09-24 01:26:57 +0700

    register link db

---
+++ gourmetguide/src/app/attractions/api_login/route.tsx
@@ -0,0 +1,20 @@
+/* import {NextResponse} from 'next/server'
+import  { mysqlPool }  from '@/app/utils/db'
+import { error } from 'console';
+
+export async function POST(req:any){
+    try {
+        const connection = await mysqlPool;
+        const {name, email, password} = await req.json();
+        console.log("Name: ", name);
+        console.log("Email: ", email);
+        console.log("Password: ", password);
+        connection.execute(`INSERT INTO account value(default, '${name}', '${password}', '${email}')`)
+        connection.end();
+        return NextResponse.json({message: "User registered."}, {status: 201});
+    } catch {error}{
+        return NextResponse.json({
+            error : error
+        }, { status: 500});
+    }
+} */
--- gourmetguide/src/app/components/Nav.tsx
+++ gourmetguide/src/app/components/Nav.tsx
@@ -7,6 +7,71 @@ import Modal from "./modal";
 function Header() {
   const [showModal, setShowModal] = useState(false);
   const [showModal2, setShowModal2] = useState(false);
+  const [errorLogin, setErrorLogin] = useState("");
+  const [errorRegister, setErrorRegister] = useState("");
+
+  const mixLogin = async () => {
+    setErrorLogin("");
+  }
+  const mixRegister = async () => {
+    setErrorRegister("");
+  }
+  const handleSubmitLogin = async (e:any) => {
+    e.preventDefault();
+    let abc:any = document.querySelector(".name")
+    let name = abc.value
+    abc = document.querySelector(".password")
+    let password = abc.value
+     if (!name || !password){
+      setErrorLogin("Please commit all form first!");
+      return;
+     }
+  }
+
+
+  const handleSubmitRegister = async (e:any) => {
+    e.preventDefault();
+    let abc:any = document.querySelector(".name")
+    let name = abc.value
+    abc = document.querySelector(".email")
+    let email = abc.value
+    abc = document.querySelector(".password")
+    let password = abc.value
+    abc = document.querySelector(".confirmPassword")
+    let confirmPassword = abc.value
+    if (password != confirmPassword){
+      setErrorRegister("Password does not match!");
+      return;
+    }
+
+    if (!name || !email || !password || !confirmPassword){
+      setErrorRegister("Please commit all form first!");
+      return;
+    }
+
+    try {
+        let res = await fetch("http://localhost:3000/attractions/api_register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            name: name, email: email, password: password
+          })
+        })
+        if (res.ok) {
+          console.log("KUY")
+          const form = e.target;
+          setErrorRegister("");
+          form.reset();
+        }else {
+          console.log("User registration failed.");
+        }
+    }catch(error) {
+      console.log("Error during registration.", error);
+    }
+  }
+
   return (
     <div>
       <div className="grid grid-cols-2"> {/* div นี้สำหรับกด nav bar */}
@@ -31,6 +96,7 @@ function Header() {
       </div>
       <Modal isVisible={showModal} onClose={() => setShowModal(false)}> {/* เนื้อหาของหน้า login */}
         <div>
+          <form onSubmit={handleSubmitLogin}>
           <div>
             <h3 className="text-black font-normal text-2xl mt-5 mb-5 flex justify-center items-center">
               เข้าสู่ระบบ
@@ -40,6 +106,7 @@ function Header() {
                 ชื่อผู้ใช้งาน:
               </h3>
               <input
+                onChange={mixLogin}
                 type="text"
                 placeholder="กรอกชื่อผู้ใช้งาน"
                 className="text-black w-96 px-4 py-2 border border-black rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center ml-6"
@@ -50,6 +117,7 @@ function Header() {
                 รหัสผ่าน:
               </h3>
               <input
+                onChange={mixLogin}
                 type="password"
                 placeholder="กรอกรหัสผ่าน"
                 className="border border-black text-black w-96 px-6 py-2 rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center"
@@ -61,19 +129,25 @@ function Header() {
               <p className="hover:underline ml-40 text-gray-500">ลืมรหัสผ่าน?</p>
             </div>
           </div>
+            {errorLogin && (
+              <div className="flex justify-center items-center bg-red-500 w-fit text-white text-sm py-1 px-3 rounded-md">{errorLogin}</div>
+            )}
           <div className="space-x-14 my-8">
             <button className="text-xl bg-yellow-500 text-white border border-black rounded-full px-12 py-1">เข้าสู่ระบบ</button>
             <button className="text-xl bg-white text-black border-2 border-yellow-500 rounded-full px-12 py-1" onClick={() => {
               setShowModal2(true);
               setShowModal(false);
+              mixLogin();
               }}>ลงทะเบียน</button> {/* ตั้งกดแล้วเปิดหน้า register ปิดหน้า login */}
           </div>
+          </form>
         </div>
       </Modal>
       <Modal isVisible={showModal2} onClose={() => setShowModal2(false)}> {/*เนื้อหาของหน้า register*/}
       <div>
+        <form onSubmit={handleSubmitRegister}>
           <div>
-            <h3 className="text-black font-normal text-2xl mt-5 mb-5 flex justify-center items-center">
+            <h3 className="text-black font-normal text-2xl mb-3 flex justify-center items-center">
               ลงทะเบียน
             </h3>
             <div className="flex flex-col items-center justify-center">
@@ -82,9 +156,10 @@ function Header() {
                   อีเมล:
                 </h3>
                 <input
-                  type="text"
+                  onChange={mixRegister}
+                  type="email"
                   placeholder="กรอกอีเมล (example@gmail.com)"
-                  className="text-black w-96 px-4 py-2 border border-black rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center ml-6"
+                  className="email text-black w-96 px-4 py-2 border border-black rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center ml-6"
                 ></input>
               </div>
               <div className='flex flex-row items-center justify-center py-2 text-yellow-500 font-normal bg-white space-x-6"'>
@@ -92,9 +167,10 @@ function Header() {
                   ชื่อผู้ใช้งาน:
                 </h3>
                 <input
+                  onChange={mixRegister}
                   type="text"
                   placeholder="กรอกชื่อผู้ใช้งาน"
-                  className="text-black w-96 px-4 py-2 border border-black rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center ml-6"
+                  className="name text-black w-96 px-4 py-2 border border-black rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center ml-6"
                 ></input>
               </div>
               <div className="flex flex-row items-center justify-center py-2 text-yellow-500 font-normal bg-white space-x-10 w-full">
@@ -103,9 +179,10 @@ function Header() {
                 </h3>
                 <div className="flex flex-col items-end mt-6">
                 <input
+                  onChange={mixRegister}
                   type="password"
                   placeholder="กรอกรหัสผ่าน (อย่างน้อย 8 หรือ 12 ตัวอักษร)"
-                  className="border border-black text-black w-96 px-4 py-2 rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center"
+                  className="password border border-black text-black w-96 px-4 py-2 rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center"
                 ></input>
                 <p className="text-normal text-gray-500 text-sm">ต้องมีตัวเลข, ตัวพิมพ์เล็ก, ตัวพิมพ์ใหญ่</p>
                 </div>
@@ -115,21 +192,26 @@ function Header() {
                   ยินยันรหัสผ่าน:
                 </h3>
                 <input
+                  onChange={mixRegister}
                   type="password"
                   placeholder="กรอกรหัสผ่าน"
-                  className="text-black w-96 px-4 py-2 border border-black rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center ml-6"
+                  className="confirmPassword text-black w-96 px-4 py-2 border border-black rounded-l-3xl rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent flex justify-center items-center ml-6"
                 ></input>
               </div>
             </div>
           </div>
+            {errorRegister && (
+              <div className="flex justify-center items-center bg-red-500 w-fit text-white text-sm py-1 px-3 rounded-md">{errorRegister}</div>
+            )}
           <div className="justify-center items-center my-8">
             <button className="text-xl bg-yellow-500 text-white border border-black rounded-full px-12 py-1">ลงทะเบียน</button>
             <div className="flex flex-row space-x-4 justify-center items-center">
               <p className="">มีบัญชีอยู่แล้ว?</p>
-              <p className="hover:underline hover:text-blue-400" onClick={() => {setShowModal2(false); setShowModal(true);}}>เข้าสู่ระบบ</p> {/* ตั้งกดแล้วปิดหน้า register เปิดหน้า login */}
+              <p className="hover:underline hover:text-blue-400" onClick={() => {setShowModal2(false); setShowModal(true); mixRegister()}}>เข้าสู่ระบบ</p> {/* ตั้งกดแล้วปิดหน้า register เปิดหน้า login */}
             </div>
           </div>
-        </div>
+          </form>
+      </div>
       </Modal>
     </div>
   );
