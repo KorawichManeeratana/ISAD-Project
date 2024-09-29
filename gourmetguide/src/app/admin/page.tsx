ade892eba326d9bcaa376434174aa399801200e4
insisapim <66070322@kmitl.ac.th>
2024-09-29 23:48:05 +0700

    logo add

--- gourmetguide/src/app/admin/page.tsx
+++ gourmetguide/src/app/admin/page.tsx
@@ -1,5 +1,10 @@
 "use client"
 import React from 'react'
+import Image from 'next/image';
+import logoAcc from '../image/logoaccount.png';
+import logoMan from '../image/logomanage.png';
+import logoRec from '../image/logorecipes.png';
+import logoRep from '../image/logoreport.png';
 import './page.css';
 import Link from 'next/link';
 
@@ -7,23 +12,29 @@ class Admin_page extends React.Component{
   render() {return (
     <div className='admin_main_page flex flex-col justify-center items-center'>
         <div className='admin_main_1'>
-          <div>account image</div>
+        <Image src={logoAcc} alt={'aun nigga'} width={100} height={100}></Image>
           <h1>username</h1>
           </div>
         <div className='admin_c'>
-          <div>manage image</div>
+          <Link href="/admin/manage_account">
+          <Image src={logoMan} alt={'aun nigga'} width={100} height={100}></Image>
+          </Link>
           <p>
           <Link href="/admin/manage_account">จัดการแอคเคาท์</Link>
           </p>
           </div>
         <div className='admin_c'>
-          <div>recipes image</div>
+          <Link href="/admin/manage_recipes">
+          <Image src={logoRec} alt={'aun nigga'} width={100} height={100}></Image>
+          </Link>
           <p>
           <Link href="/admin/manage_recipes">จัดการสูตรอาหาร</Link>
           </p>
           </div>
         <div className='admin_c'>
-          <div>reports image</div>
+          <Link href="/admin/report_feedback">
+            <Image src={logoRep} alt={'aun nigga'} width={100} height={100}></Image>
+          </Link>
           <p>
           <Link href="/admin/report_feedback">รับเรื่องที่แจ้ง</Link>
           </p>
