b056f22c392cd953cc827404c29421b1a15fd5e7
Best <141381218+KorawichManeeratana@users.noreply.github.com>
2024-10-20 20:58:10 +0700

    Last

--- gourmetguide/src/app/utils/database/dbConnect.ts
+++ gourmetguide/src/app/utils/database/dbConnect.ts
@@ -5,7 +5,7 @@ dotenv.config()
 export default class dbConnector {
     private connection: Pool | undefined;
     private setting: PoolOptions = {
-        host: process.env.DB_HOSTPKRIT,
+        host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_DATABASE,
